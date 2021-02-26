adcf9e4a2e3f03ada4bb05fe9ead7442bc81987b
Thomas Yriarte <thomas.yriarte@etu.isima.fr>
2021-02-26 15:12:07 +0100

    searchbar

---
+++ frontend/src/components/service/Option.js
@@ -0,0 +1,8 @@
+import './Option.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function Option({Name}) {
+    return (<option value={Name}/>);
+}
+
+export default Option;
--- frontend/src/components/service/Services.js
+++ frontend/src/components/service/Services.js
@@ -1,6 +1,7 @@
 
 import { useState, useEffect } from 'react';
 import Service from './Service.js'
+import Option from './Option';
 
 function Services() {
 
@@ -15,14 +16,43 @@ function Services() {
 	  key={service.serviceId}
 	/>);
 
+	const [timer, setTimer] = useState(null);
+	const [previousText, setPreviousText] = useState(null);
+
+	let OptionList = serviceData.map(service => <Option Name={service.name} key={service.serviceId}/>);
+	
+	const [searchText, setSearchText] = useState(null);
+  	const SearchbarFunction = txt => {
+                                fetch('https://localhost:44398/service_name?name='+txt)
+                                  .then(response => response.json())
+								  .then(json => setServiceData(json));
+  	}
+
 	return (
 		<div className="row">
-		  <h1>services</h1>
-          <div className="col-1">serviceid</div>
-          <div className="col-1">name</div>
-          <div className="col-4">adress</div>
-          <h1> </h1>
-          {ServiceList}
+		  	<h1>services</h1>
+
+		  	<div className="country-search">
+				<label for="exampleDataList" className="form-label">Search by country</label>
+				<input className="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search..."
+				onChange={e => {
+					if (previousText !== e.target.value) {
+						clearTimeout(timer);
+						const timeoutId = setTimeout(() => SearchbarFunction(e.target.value), 1000);
+						setTimer(timeoutId);
+						setPreviousText(e.target.value);
+					}
+					}}/>
+				<datalist id="datalistOptions">
+					{OptionList}
+				</datalist>
+        	</div>
+
+			<div className="col-1">serviceid</div>
+			<div className="col-1">name</div>
+			<div className="col-4">adress</div>
+			<h1> </h1>
+			{ServiceList}
         </div>
 	);
 }
