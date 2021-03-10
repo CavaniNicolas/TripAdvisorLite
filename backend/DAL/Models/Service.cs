bdb53b21dec3ad9f92dc5116003490dbc4b87036
Cavani Nicolas <cavani.nicolas6@gmail.com>
2021-03-10 17:02:32 +0100

    rename folder

---
+++ backend/DAL/Models/Service.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Models
+{
+    public class Service
+    {
+        public int ServiceId { get; set; }
+        public string Adress { get; set; }
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+        //public List<Review> ReviewList { get; set; }
+
+        public Service()
+        {
+            ServiceId = 0;
+            Name = " ";
+            Adress = " ";
+            Type = "";
+        }
+
+        public override string ToString()
+        {
+            return this.ServiceId + " " + this.Name + " " + this.Adress;
+        }
+    }
+}
---
+++ backend/DAL/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        //public List<Review> ReviewList { get; set; }
+
+        public User()
+        {
+            UserId = 0;
+            Username = " ";
+            //ReviewList = new List<Review>();
+        }
+
+        public override string ToString()
+        {
+            return this.UserId + " " + this.Username;
+        }
+    }
+}
